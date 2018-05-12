c69d87c6b84451823dc392dc3995eb7be00feec0
Soumithsai <soumith.github@gmail.com>
2018-05-11 22:36:57 -0500

    exception

---
+++ ScalaException/src/scalaException.scala
@@ -0,0 +1,13 @@
+import java.io._
+import io.Source._
+
+object scalaException {
+  def main(args:Array[String]){
+    try {
+      val x = fromFile("src/sss.txt").getLines()
+    }catch{
+      /*case e:FileNotFoundException => println("Check your file")*/
+      case _:Exception => println("Check your program")
+    }
+  }
+}
