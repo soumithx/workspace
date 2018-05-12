1d1ac973fdd000e761c2c79c8df02490fb3b04aa
Soumithsai <soumith.github@gmail.com>
2018-05-11 22:28:02 -0500

    input

--- System_Input/src/SourceFile.scala
+++ System_Input/src/SourceFile.scala
@@ -1,9 +1,11 @@
 import io.Source._
 object SourceFile {
   def main(args:Array[String]){
- val x = "src/example.txt"
- for(i <-  fromFile(x).getLines()){
-   println(i)
- }
-}
+   val fileName = "src/example.txt"
+   for(line <- fromFile(fileName).getLines()){
+     println(line)
+   }
+   val pom = fromFile("src/lipsum.txt").getLines().toList
+   pom.foreach(println)
+  }
 }
