10f6daeb27fd59db7637f6e58075ab5ca9b2ee98
Soumithsai <soumith.github@gmail.com>
2018-05-11 21:53:32 -0500

    system input

---
+++ SystemInput.scala
@@ -0,0 +1,15 @@
+import scala.io.StdIn._
+object SystemInput extends App {
+     def main(args:String){
+            var x = false
+                 while (!x) {
+                          var name = readLine("Enter the name")
+                                 println("Enter the score")
+                                        var l = readDouble()
+                                               println("you recieved "+(if (l>90) "Your Grade is A" else if(l>80) "Your Grade is B"
+                                                        else if (l>70) "Your Grade is C" else  "Your Fail"))
+                                                                  println("Do you want to continue 'yes' for False, 'no' for  True")
+                                                                          x = readBoolean()
+                                                                                 }
+                                                                                    }
+}
